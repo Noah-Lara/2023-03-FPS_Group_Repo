088764944ffe2ad1d6e6dea9c59b4feddea4bd03
Caballer12 <kjcaballerortiz@student.fullsail.edu>
2023-03-08 12:28:48 -0500

    playerMovement & Camera
    playerMovement & Camera

---
+++ Group FPS Project/Assets/Scripts/cameraControls.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraControls : MonoBehaviour
+{
+    [Range(2000, 4000)][SerializeField] int sensHor;
+    [Range(2000, 4000)] [SerializeField] int sensVert;
+
+    [SerializeField] int lockVertMin;
+    [SerializeField] int lockVertMax;
+
+    [SerializeField] bool invertY;
+
+    float xRotation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //makes cursor invisible
+        Cursor.visible = false;
+        //keeps cursor inside the game
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseY = Input.GetAxis("Mouse Y") * Time.deltaTime * sensVert;
+        float mouseX = Input.GetAxis("Mouse X") * Time.deltaTime * sensHor;
+
+        if (invertY)
+            xRotation += mouseY;
+        else
+            xRotation -= mouseY;
+
+        //xRotation += mouseY;
+
+        //clamp the camera rotation
+        xRotation = Mathf.Clamp(xRotation, lockVertMin, lockVertMax);
+
+        //rotate the camera on the X-axis 
+        transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+
+        //rotate the player on its Y-axis
+        transform.parent.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ Group FPS Project/Assets/Scripts/playerController.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    [Header("-----Components-----")]
+    [SerializeField] CharacterController controller;
+    [Header("-----Player Stats-----")]
+    [Range(1, 15)] [SerializeField] float playerSpeed;
+    [Range(1, 4)] [SerializeField] int jumpTimes;
+    [Range(1, 15)] [SerializeField] int jumpSpeed;
+    [Range(1, 70)] [SerializeField] int gravity;
+
+    [Header("-----Gun Stats-----")]
+    [SerializeField] float shootRate;
+    [SerializeField] int shootDist;
+    [SerializeField] int shootDamage;
+    [SerializeField] GameObject cube;
+
+    int jumpCurrent;
+    Vector3 move;
+    Vector3 playerVelocity;
+    bool isShooting;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement();
+        //if (!gameManager.instance.isPaused)//Fix the bug when player can shoot or jump once after pausing
+        //{
+        //    movement();
+        //    if (!isShooting && Input.GetButton("Shoot"))
+        //    {
+        //        StartCoroutine(shoot());
+        //    }
+        //}
+    }
+    //Movement settings
+    void movement()
+    {
+        if (controller.isGrounded && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0;
+            jumpCurrent = 0;
+        }
+
+        move = transform.right * Input.GetAxis("Horizontal") + transform.forward * Input.GetAxis("Vertical");
+        move = move.normalized;
+
+        controller.Move(move * Time.deltaTime * playerSpeed);
+
+        if (Input.GetButtonDown("Jump") && jumpCurrent < jumpTimes)
+        {
+            jumpCurrent++;
+            playerVelocity.y = jumpSpeed;
+        }
+        playerVelocity.y -= gravity * Time.deltaTime;
+
+        controller.Move(playerVelocity * Time.deltaTime);
+
+        //Debug.Log(move);//TrackPlayer Movement Speed
+    }
+
+    //IEnumerator shoot()
+    //{
+    //    isShooting = true;
+
+    //    RaycastHit hit;
+    //    if (Physics.Raycast(Camera.main.ViewportPointToRay(new Vector2(0.5f, 0.5f)), out hit, shootDist))
+    //    {
+    //        if (hit.collider.GetComponent<IDamage>() != null)
+    //        {
+    //            hit.collider.GetComponent<IDamage>().takeDamage(shootDamage);
+    //        }
+    //        // Instantiate(cube, hit.point, transform.rotation);//instantiate a cube where player is looking
+    //    }
+    //    yield return new WaitForSeconds(shootRate);
+    //    isShooting = false;
+    //}
+}
