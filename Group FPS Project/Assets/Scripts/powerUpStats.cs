636eeacfa501c1bb03ed02cf8e5cfcf1f5107b5d
AMMayles <ammayles@student.fullsail.edu>
2023-03-22 15:36:10 -0400

    Added explosion Power-up pickup and stats
    Create a gameobject that will act as an explosion powerup

---
+++ Group FPS Project/Assets/Scripts/powerUpStats.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+
+public class powerUpStats : ScriptableObject
+{
+    public float shootRate;
+    public int shootDist;
+    public int shootDamage;
+}
