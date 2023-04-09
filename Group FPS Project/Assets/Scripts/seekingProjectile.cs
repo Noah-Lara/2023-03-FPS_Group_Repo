45f9d2c9a80ad96dae0f3393956162a4fc8ed1bc
Caballer12 <kjcaballerortiz@student.fullsail.edu>
2023-04-09 04:09:10 -0400

    seekingProjectile update
    the projectile can only see you are within 90 degree sight angle

--- Group FPS Project/Assets/Scripts/seekingProjectile.cs
+++ Group FPS Project/Assets/Scripts/seekingProjectile.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 public class seekingProjectile : MonoBehaviour
 {
     [SerializeField] float speed;
+    //[SerializeField] int Angle;
+    [SerializeField] int sightAngle;
+    Vector3 playerDir;
+    float angleToTarget;
+    
+    [SerializeField] Transform headPos;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +21,13 @@ public class seekingProjectile : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector3.MoveTowards(transform.position, gameManager.instance.player.transform.position, speed * Time.deltaTime);
+        playerDir = (gameManager.instance.player.transform.position - headPos.position);
+        angleToTarget =  Vector3.Angle(new Vector3(playerDir.x, 0, playerDir.z), transform.forward);
+        if(angleToTarget <= sightAngle)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, gameManager.instance.player.transform.position, speed * Time.deltaTime);
+            transform.forward = gameManager.instance.player.transform.position - transform.position;
+        }
+        
     }
 }
