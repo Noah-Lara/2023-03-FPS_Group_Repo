384816dce26c8017bd9b15e753d99a014470b0c6
AMMayles <ammayles@student.fullsail.edu>
2023-04-12 21:24:02 -0400

    Changed when the button sound plays

--- Group FPS Project/Assets/Scripts/buttonSound.cs
+++ Group FPS Project/Assets/Scripts/buttonSound.cs
@@ -3,15 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class buttonSound : MonoBehaviour, ISelectHandler, IPointerEnterHandler
+public class buttonSound : MonoBehaviour, ISelectHandler
 {
     public AudioSource aud;
     [SerializeField] AudioClip audClip;
-
-    public void OnPointerEnter(PointerEventData eventData)
-    {
-        aud.PlayOneShot(audClip);
-    }
 
     public void OnSelect(BaseEventData eventData)
     {
